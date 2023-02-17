7f54fbf508ce54cc084c66e337021dafe64ef692
Patrick Maciel <patrickmaciel.info@gmail.com>
2023-02-17 11:23:30 -0300

    episode 2: unit testing a simple class

---
+++ example-1/src/TagParser.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+class TagParser
+{
+    public function parse(string $tags)
+    {
+//        return explode(', ', $tags);
+        return preg_split('/ ?[,|] ?/', $tags);
+//        return array_map(fn($tag) => trim($tag), $tags);
+
+//        return array_map(function ($tag) {
+//           return trim($tag);
+//        }, $tags);
+    }
+}
--- example-1/tests/TagParserTest.php
+++ example-1/tests/TagParserTest.php
@@ -1,16 +1,57 @@
 <?php
 
 namespace Tests;
+use App\TagParser;
 use PHPUnit\Framework\TestCase;
 
 class TagParserTest extends TestCase
 {
     // personal, money, family
-    public function test_it_parses_a_comma_separated_list_of_tags()
+    public function test_it_parses_a_single_tag()
     {
-        $parser = new TagParser;
+        $parser = new TagParser();
+
+        $result = $parser->parse('personal');
+        $expected = ['personal'];
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_it_parses_a_comma_with_space_separated_lisf_of_tags()
+    {
+        $parser = new TagParser();
 
         $result = $parser->parse('personal, money, family');
+        $expected = ['personal', 'money', 'family'];
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_it_parses_a_comma_separated_lisf_of_tags()
+    {
+        $parser = new TagParser();
+
+        $result = $parser->parse('personal,money,family');
+        $expected = ['personal', 'money', 'family'];
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_it_parses_a_pipe_separeted_lists_of_tags()
+    {
+        $parser = new TagParser();
+
+        $result = $parser->parse('personal | money | family');
+        $expected = ['personal', 'money', 'family'];
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function test_it_parses_a_pipe_no_spaces_separeted_lists_of_tags()
+    {
+        $parser = new TagParser();
+
+        $result = $parser->parse('personal|money|family');
         $expected = ['personal', 'money', 'family'];
 
         $this->assertSame($expected, $result);
