e65777282ad7cc311f1f6144794e7cb631e29c78
Patrick Maciel <patrickmaciel.info@gmail.com>
2023-02-24 20:27:29 -0300

    creating a stub as a class

--- example-2/tests/SubscriptionTest.php
+++ example-2/tests/SubscriptionTest.php
@@ -21,8 +21,9 @@ class SubscriptionTest extends TestCase
      */
     function creating_a_abuscription_marks_the_user_as_subscribed()
     {
-        $gateway = $this->createMock(Gateway::class);
-        $gateway->method('create')->willReturn('receipt-stub');
+//        $gateway = $this->createMock(Gateway::class);
+//        $gateway->method('create')->willReturn('receipt-stub');
+        $gateway = new GatewayStub();
 
         $subscription = new Subscription($gateway);
 
