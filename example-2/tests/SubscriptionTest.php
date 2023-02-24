c7366919fdcf5114da7c9be54c01e0c81eada98a
Patrick Maciel <patrickmaciel.info@gmail.com>
2023-02-24 20:22:25 -0300

    creating a mock

--- example-2/tests/SubscriptionTest.php
+++ example-2/tests/SubscriptionTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests;
 
+use App\Gateway;
 use App\Subscription;
 use App\User;
+use PHPUnit\Framework\MockObject\Exception;
 use PHPUnit\Framework\TestCase;
 
 class SubscriptionTest extends TestCase
@@ -14,11 +16,12 @@ class SubscriptionTest extends TestCase
         $this->markTestSkipped();
     }
 
-    /** @test */
+    /** @test
+     * @throws Exception
+     */
     function creating_a_abuscription_marks_the_user_as_subscribed()
     {
-        $gateway = new FakeGateway(); // Mocking the gateway
-        $subscription = new Subscription($gateway);
+        $subscription = new Subscription($this->createMock(Gateway::class));
         $user = new User('Jesus');
         $this->assertFalse($user->isSubscribed());
 
